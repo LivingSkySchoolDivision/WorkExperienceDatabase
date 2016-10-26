e0dcc1dc60e99e2a7dc061ba6136999435a6a8df
Mark Strendin <mark.strendin@lskysd.ca>
2016-10-26 15:45:17 -0600

    Added a class for ActivePosition

---
+++ CareerWorkExperienceDatabase/ActivePosition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CareerWorkExperienceDatabase
+{
+    public class ActivePosition
+    {
+        public int ID { get; }
+        public int PositionID { get; }
+        public string StudentName { get; }
+        public DateTime Start { get; }
+        public DateTime End { get; }        
+    }
+}
